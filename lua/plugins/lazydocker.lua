0a7227f0fb09f2feffaac30a0667871d7bb058ef
sanjib kumar sen <sanjib@technometrics.net>
2025-07-19 03:50:34 +0600

    sanjib's changes

---
+++ lua/plugins/lazydocker.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "crnvl96/lazydocker.nvim",
+    opts = {
+      window = {
+        settings = {
+          width = 0.918, -- Percentage of screen width (0 to 1)
+          height = 0.918, -- Percentage of screen height (0 to 1)
+          border = "rounded", -- See ':h nvim_open_win' border options
+          relative = "editor", -- See ':h nvim_open_win' relative options
+        },
+      },
+    },
+  },
+}
